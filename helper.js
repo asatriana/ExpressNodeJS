047c745aeb483a2c4dec16ef414148dcaf2db2e5
asatriana <asatriana@gmail.com>
2021-12-09 21:04:59 +0700

    contoh helper class javascript

---
+++ helper.js
@@ -0,0 +1,5 @@
+const login = function(data){
+    return `${data} is logged in`;
+};
+
+module.exports = login; // mandatory to express function
---
+++ tesHelper.js
@@ -0,0 +1,8 @@
+const login = require('./helper');
+const login2 = require('./helper2');
+
+console.log(login("Aries"));
+
+console.log(login2.user("Mr EDUX"));
+console.log(login2.id(1));
+console.log(login2.email("mister@edux.org"));
