3dbf4b0acbcbf7a5c0b9ff9a1b7902f7e1692922
asatriana <asatriana@gmail.com>
2021-12-09 21:30:10 +0700

    routing in nodejs

---
+++ streambuff/approuting.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req,res)=>{
+    console.log(req.url);
+    if (req.url == '/home'|| req.url =='/'){
+        res.writeHead(200,{'Content-type':'text/html'});
+        const readStream = fs.createReadStream(__dirname + '/index.html', 'UTF-8');
+        readStream.pipe(res);
+    } else if (req.url == '/about'){
+        res.writeHead(200,{'Content-type':'text/html'});
+        fs.createReadStream(__dirname + '/about.html', 'UTF-8').pipe(res);
+    } else {
+        res.writeHead(200,{'Content-type':'text/html'});
+        fs.createReadStream(__dirname + '/404.html', 'UTF-8').pipe(res);
+    }
+    
+}); 
+
+server.listen(3000,'127.0.0.1');
